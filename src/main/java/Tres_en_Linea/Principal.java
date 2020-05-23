c45d5e3ee7b7e4fcf260eba53efeb47fd297a579
NeftaGP <47460647+NeftaGP@users.noreply.github.com>
2020-05-22 20:43:26 -0600

    correccion
    nuevos archivos

--- src/main/java/Tres_en_Linea/Principal.java
+++ src/main/java/Tres_en_Linea/Principal.java
@@ -1,7 +1,9 @@
-Error
-
-reading included file Templates/Licenses/license-default.txt
-package Clases;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tres_en_Linea;
 
 /**
  *
@@ -14,6 +16,7 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
