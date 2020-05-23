011b88b074e488114ba81e3b83c06f641405dcfa
NeftaGP <47460647+NeftaGP@users.noreply.github.com>
2020-05-22 20:20:49 -0600

    Inicial
    clase main y pantalla principal para proyecto

---
+++ src/main/java/Clases/Principal.java
@@ -0,0 +1,19 @@
+Error
+
+reading included file Templates/Licenses/license-default.txt
+package Clases;
+
+/**
+ *
+ * @author nefta
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/main/java/Clases/pagina.java
@@ -0,0 +1,12 @@
+Error
+
+reading included file Templates/Licenses/license-default.txt
+package Clases;
+
+/**
+ *
+ * @author nefta
+ */
+public interface pagina {
+    
+}
